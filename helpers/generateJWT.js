8daacdeec8d88ae53bc7e98439eb7ac140d7b5e6
Yactayo <syactayocrisostomo@gmail.com>
2022-05-03 19:23:13 -0500

    [ADD] controller img

--- helpers/generateJWT.js
+++ helpers/generateJWT.js
@@ -1,19 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const generateJWT = (uid, name) => {
+  const payload = { uid, name };
 
-    const payload = { uid, name };
- 
-    try {
-        const token = jwt.sign(payload, process.env.SECRET_JWT_SEED, {
-            expiresIn: '1h',
-        });
-        return token;
-    } catch (err) {
-        console.log(err);
-        return 'No se pudo generar el token';
-    }
+  // Si vas a usar process, crea un .env
+  try {
+    const token = jwt.sign(payload, process.env.SECRET_JWT_SEED, {
+      expiresIn: "1h",
+    });
+    return token;
+  } catch (err) {
+    console.log(err);
+    return "No se pudo generar el token";
+  }
+};
 
-}
-
-module.exports = { generateJWT }
+module.exports = { generateJWT };
--- models/User.js
+++ models/User.js
@@ -1,39 +1,56 @@
-const { Schema, model } = require('mongoose');
-const { generateJWT } = require('../helpers/generateJWT');
+const { Schema, model } = require("mongoose");
+const { generateToken } = require("../helpers/generateToken");
 
 const UserSchema = Schema({
-    card: {
-        type: String,
-        required: false,
-    },
-    fullname: {
-        type: String,
-        required: true,
-    },
-    dni: {
-        type: Number,
-        required: false,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    photo: {
-        type: String,
-        required: false,
-    },
-    phone: {
-        type: Number,
-        required: false,
-    },
-    confirmed: {
-        type: Boolean,
-        default: false,
-    },
-}); 
+  card: {
+    type: String,
+    required: false,
+  },
 
-module.exports = model('User', UserSchema);
+  fullname: {
+    type: String,
+    required: false,
+  },
+
+  dni: {
+    type: Number,
+    required: false,
+  },
+
+  email: {
+    type: String,
+    required: false,
+  },
+
+  password: {
+    type: String,
+    required: false,
+  },
+
+  photo: {
+    type: String,
+    required: false,
+  },
+
+  phone: {
+    type: Number,
+    required: false,
+  },
+
+  token: {
+    type: String,
+    default: generateToken(),
+  },
+
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UserSchema.methods.setImgUrl = function setImgUrl(filename) {
+  const HOST = process.env.HOST || "localhost:4000";
+  this.photo = `${HOST}/public/${filename}`;
+};
+
+module.exports = model("User", UserSchema);
